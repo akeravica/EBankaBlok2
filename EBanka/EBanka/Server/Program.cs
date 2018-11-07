2122c3f96945c8916ab11ae88256bae6691536ec
Brana <branislava.karavidin@gmail.com>
2018-11-07 20:40:31 +0100

    Added roles and permissions.

---
+++ EBanka/EBanka/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/Operacije";
+
+            ServiceHost host = new ServiceHost(typeof(Operacije));
+            host.AddServiceEndpoint(typeof(IOperacije), binding, address);
+
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
+
+            host.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+            Console.ReadLine();
+
+            host.Close();
+        }
+    }
+}
